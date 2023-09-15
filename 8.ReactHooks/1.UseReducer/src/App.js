49e58a9250deab2194dc3d5068f5bf5acb6b56d7
Serhat OMAÇ <omacserhatt@gmail.com>
2023-09-15 15:42:19 +0300

    UseReducer added

---
+++ 8.ReactHooks/1.UseReducer/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { useReducer } from 'react';
+
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialValue;
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [count, dispatch] = useReducer(reducer, initialValue);
+  return (
+    <div className="App">
+      <div>Sayı = {count}</div>
+      <button onClick={() => dispatch('increment')}>Arttır</button>
+      <button onClick={() => dispatch('decrement')}>Azalt</button>
+      <button onClick={() => dispatch('reset')}>Sıfırla</button>
+    </div>
+  );
+}
+
+export default App;
