97f20d539e71696e471fe66cd32bdd03503e8ffb
Serhat OMAÇ <omacserhatt@gmail.com>
2023-09-15 16:02:55 +0300

    UseReducer + UseContext added

---
+++ 8.ReactHooks/2.UseReducerAndUseContext/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useEffect, useReducer } from 'react';
+import Calculate from './Calculate';
+
+export const NumberContext = React.createContext();
+
+debugger;
+const initialValue = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialValue;
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [count, dispatch] = useReducer(reducer, initialValue);
+
+  useEffect(() => {
+    console.log('Render oldu');
+  }, [count]);
+  return (
+    <div className="App">
+      <NumberContext.Provider value={{ count: count, dispatch: dispatch }}>
+        <Calculate />
+      </NumberContext.Provider>
+    </div>
+  );
+}
+
+export default App;
